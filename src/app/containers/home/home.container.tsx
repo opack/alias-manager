a1804150ba962abff64c55e9bffc4429c9048aea
Opack <gitkraken@valdera.fr>
2019-11-10 18:07:58 +0100

    Version without redux

--- src/app/containers/home/home.container.tsx
+++ src/app/containers/home/home.container.tsx
@@ -1,20 +1,17 @@
-import React from 'react'
-import { useSelector, useDispatch } from 'react-redux';
+import React, { useState, useEffect } from 'react'
 
-import { GlobalStore } from '../../reducers/store';
-import { addAlias, updateFilter } from '../../reducers/aliases.reducer';
+import {fetchAliases} from '../../services/redirections.service';
 
-import Filter from "../../components/filter/filter.component";
-import AliasList from "../../components/alias-list/alias-list.component";
-import AliasCreator from "../../components/alias-creator/alias-creator.component";
+import Filter from '../../components/filter/filter.component';
+import AliasList from '../../components/alias-list/alias-list.component';
+import AliasCreator from '../../components/alias-creator/alias-creator.component';
 import AliasData from '../../services/@data-types/alias-data';
 
 const Home = () => {
-    const { filter, aliases } = useSelector((state: GlobalStore) => state.aliases);
+    const [aliases, setAliases] = useState(new Array<AliasData>());
+    const [filter, setFilter] = useState('');
 
-    const dispatch = useDispatch();
-    const handleUpdateFilter = (term: string) => dispatch(updateFilter(term));
-    const handleAddAlias = (from: string) => dispatch(addAlias(new AliasData('', from, '')));
+    const addAlias = (from: string) => setAliases(aliases.concat(new AliasData('', from, '')));
 
     const getVisibleAliases = (aliases: AliasData[], filter: string): AliasData[] => {
         // If there is a filter, then we filter the list of data to only keep the ones matching the filter
@@ -25,11 +22,20 @@ const Home = () => {
         return aliases;
     };
 
+    const refreshAliases = () => {    
+        fetchAliases().then((fetchedAliases: Array<AliasData>) => {
+            setAliases(fetchedAliases);
+        });
+    }
+
+    useEffect(() => refreshAliases(), []);
+
     return (
         <div>
-            <Filter initialFilterTerm={filter} filter={handleUpdateFilter} />
+            <Filter initialFilterTerm={filter} filter={setFilter} />
             <AliasList aliases={getVisibleAliases(aliases, filter)} />
-            <AliasCreator onCreateAliasClick={handleAddAlias} />
+            <AliasCreator onCreateAliasClick={addAlias} />
+            <button onClick={refreshAliases} >Refresh</button>
         </div>
     );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,36 +1,24 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
 
 import * as serviceWorker from './serviceWorker';
 
 import './index.css';
-import store from './app/reducers/store';
-//import { fetchAliases } from './app/reducers/aliases.reducer';
 import requestCredentials from './app/services/ovh-credentials.service';
-
-import {fetchAliases} from './app/services/redirections.service';
-import AliasData from './app/services/@data-types/alias-data';
-import { populateAliases } from './app/reducers/aliases.reducer';
 
 const render = () => {
     const App = require('./app/components/app/app').default
 
     ReactDOM.render(
-        <Provider store={store}>
+        <Fragment>
             <App />
             <button onClick={requestCredentials}>Request credentials</button>
-        </Provider>,
+        </Fragment>,
         document.getElementById('root')
     )
 }
 
 render();
-
-// DBG Fetch alias list
-fetchAliases().then((fetchedAliases: Array<AliasData>) => {
-    store.dispatch(populateAliases(fetchedAliases));
-});
 
 if (process.env.NODE_ENV === 'development' && module.hot) {
     module.hot.accept('./app/components/app/app', render)
