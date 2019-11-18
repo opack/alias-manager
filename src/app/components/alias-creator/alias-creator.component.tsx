73bf6681b106234054e7ebb363c638b562b10546
Opack <gitkraken@valdera.fr>
2019-11-18 23:34:05 +0100

    Handle domain properly

--- src/app/components/alias-creator/alias-creator.component.tsx
+++ src/app/components/alias-creator/alias-creator.component.tsx
@@ -1,28 +1,29 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
 import AliasCreatorProps from './alias-creator.props';
 import aliasCreatorDefaultProps from './alias-creator.default-props';
 
-const AliasCreator = ({ domain, onCreateAliasClick }: AliasCreatorProps) => {
-  const [alias, setAlias] = useState<string>('');
-  const updateAlias = (event: any) => setAlias(event.target.value);
-  const createAlias = () => onCreateAliasClick(`${alias}@valdera.fr`, 'marekh.ebony@gmail.com');
+const AliasCreator = ({domain, onCreateAliasClick}: AliasCreatorProps) => {
+    const [alias, setAlias] = useState<string>('');
+    const updateAlias = (event: any) => setAlias(event.target.value);
+    const createAlias = () => onCreateAliasClick(`${alias}@${domain}`, 'marekh.ebony@gmail.com');
 
-  return (
-    <div>
-      <label className="label">Create new alias</label>
-      <div className="field has-addons">
-        <div className="control">
-          <input className="input has-addons" type="text" value={alias} onChange={updateAlias} placeholder="some-alias"/>
+    return (
+        <div>
+            <label className="label">Create new alias</label>
+            <div className="field has-addons">
+                <div className="control">
+                    <input className="input has-addons" type="text" value={alias} onChange={updateAlias}
+                           placeholder="some-alias"/>
+                </div>
+                <div className="control">
+                    <button className="button" disabled><span>@{domain}</span></button>
+                </div>
+                <button className="button is-primary" onClick={createAlias}>Create</button>
+            </div>
+
         </div>
-        <div className="control">
-          <button className="button" disabled><span>@{domain}</span></button>
-        </div>
-        <button className="button is-primary" onClick={createAlias}>Create</button>
-      </div>
-      
-    </div>
-  );
+    );
 };
 
 AliasCreator.defaultProps = aliasCreatorDefaultProps;
