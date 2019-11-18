020289a3429c2a125182fe4e4c289a80413cb2af
Opack <gitkraken@valdera.fr>
2019-11-19 00:20:07 +0100

    Add tabs and other UI enhancements

--- src/app/components/alias-list/alias-list.component.tsx
+++ src/app/components/alias-list/alias-list.component.tsx
@@ -4,32 +4,32 @@ import Alias from '../alias/alias.component';
 import AliasListDefaultProps from './alias-list.default-props';
 import AliasData from '../../services/@data-types/alias-data';
 
-const AliasList = ({ aliases, onRemoveAliasClick, onRefreshList, filterComponent }: AliasListProps) => {
-  return (
-    <div>
-        <label className="subtitle">Alias list <span className="tag">{aliases.length}</span></label>
-        <button className="button" onClick={onRefreshList}>Refresh</button>
-        { filterComponent }
-        <table className="table is-striped">
-            <thead>
+const AliasList = ({aliases, onRemoveAliasClick, onRefreshList, filterComponent}: AliasListProps) => {
+    return (
+        <div>
+            <label className="label">Alias list <span className="tag">{aliases.length}</span></label>
+            <button className="button" onClick={onRefreshList}>Refresh</button>
+            {filterComponent}
+            <table className="table is-striped">
+                <thead>
                 <th>From</th>
                 <th>To</th>
                 <th>Actions</th>
-            </thead>
-            <tbody>
-              {
-                aliases.map( (alias: AliasData) => {
-                  return (
-                    <tr key={alias.id}>
-                      <Alias {...alias} onRemoveAliasClick={onRemoveAliasClick}/>
-                    </tr>
-                  );
-                })
-              }
-            </tbody>
-        </table>
-    </div>
-  );
+                </thead>
+                <tbody>
+                {
+                    aliases.map((alias: AliasData) => {
+                        return (
+                            <tr key={alias.id}>
+                                <Alias {...alias} onRemoveAliasClick={onRemoveAliasClick}/>
+                            </tr>
+                        );
+                    })
+                }
+                </tbody>
+            </table>
+        </div>
+    );
 };
 
 AliasList.defaultProps = AliasListDefaultProps;
